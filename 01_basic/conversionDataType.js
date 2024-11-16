541e3328d6c245b67ccf368db716a5f915304b55
sheikh4200 <159758352+sheikh4200@users.noreply.github.com>
2024-11-16 09:28:55 +0000

    conveerrsiion 0f DataTypes

---
+++ 01_basic/conversionDataType.js
@@ -0,0 +1,31 @@
+// let start this
+
+// let number = 23
+
+// let convertToString = String(number)
+// console.log(typeof number)
+// console.log(typeof convertToString)
+
+// let string = "sheikh"
+
+// let convertToNumber = Number(string)
+// console.log(typeof convertToNumber)
+
+// let number = false
+
+// let convertToNumber = Number(number)
+// console.log(typeof (convertToNumber))
+// console.log(convertToNumber)
+
+let b = 0
+
+let IsBoolean = Boolean(b)
+console.log(IsBoolean)
+
+//notes
+// "33" => 33 when convert to number
+// 33 => "33" when convert to string
+//"33abc" => NaN its tells that ist not a number
+//null => 0
+//undefined => NaN
+//boolean => true(1) an false(0)

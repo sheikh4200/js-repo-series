b1f0695406cc22330b8e183ca924372fcddc8027
sheikh4200 <159758352+sheikh4200@users.noreply.github.com>
2024-11-16 07:41:39 +0000

    01_variables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,10 @@
+
+const name= "sheikh";
+let name1 = 12113
+var name2 = true
+
+// const cannot modify or update
+// perfer not used var 
+// the reason is that it cannot understand scope and functional scope
+
+console.table([name,name1,name2])

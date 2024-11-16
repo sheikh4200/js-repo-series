569b4a9f38ff78d11cc9cceb8a2ae5799dd58b76
sheikh4200 <159758352+sheikh4200@users.noreply.github.com>
2024-11-16 08:07:10 +0000

    datatypes disscusion

---
+++ 01_basic/02_DataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer version
+// today  we are deep dive in dataTypes
+// there are few but mostly used DataTypes
+
+//string => ""
+let abc = "sheikh"
+//number// 2 the power of 53
+let ab = 12
+//bigint
+//undefined => undefined
+//null // is an object and standalone value
+let name = null
+ //boolean =. true or false
+ //symbol => unique
+//object
+//class
+//array
+
+// to change datatype of any like string
+console.log(typeof abc)
+console.log(typeof null)
+
+console.log(typeof ab)
+// console.log(abc)
+// console.log(abc)
+

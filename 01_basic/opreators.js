c6f2fa1015183b3836593bbbbed92cf70d408aba
sheikh4200 <159758352+sheikh4200@users.noreply.github.com>
2024-11-19 13:16:36 +0000

    solve problems and learn about operators

---
+++ 01_basic/opreators.js
@@ -0,0 +1,42 @@
+//**************//Operators//****** */
+//today we are goning learn about operators in javascript
+//let begin 
+
+// let  num = 12;
+// let num2 =13;
+//first problem 
+// let res = num + num2
+// console.log(res)
+///second problem  using + - / in a code that answer is = to 18
+// let num1 = 12;
+// let num2 = 23;
+// let res = num1 + num2 - 17;
+// console.log(res)
+
+//third problem find a increment of number
+// function increment(a,b){
+//     let res = a + b
+//  setInterval(() => {
+//     console.log(res)
+//  }, 1000
+// );   
+// }
+// increment(1,2)
+
+//four problem is to create aconcat between string and number
+
+// let str = "sheikh";
+// let num = 12
+// let res = str + num;
+// console.log(res)
+
+//no five problem is find a number of null property
+// let empty = null; 
+// let abc = String(empty) 
+// console.log(abc)
+// console.log(typeof abc)
+
+// var Word = null 
+// let isBoolean = Boolean(Word)
+// console.log(isBoolean)
+// console.log(typeof isBoolean)

ea47d6125e866888761c9f4f321bc1635d3dd0af
sheikh4200 <159758352+sheikh4200@users.noreply.github.com>
2024-11-20 13:17:38 +0000

    premittive and non-premittive /stack and heap talk

---
+++ 01_basic/premitiveandnonopremitive.js
@@ -0,0 +1,49 @@
+//today we are gonig learn about premittive and non-premittive//
+//(1)Premittive
+//srtring , number , boolean , null, undefined, BigInt,symbol
+// let age = 12;
+// let age1 = null;
+// abc = Number(age1)
+// console.log(abc)
+// console.log(typeof age1)
+// let age2 = undefined;
+// console.log(typeof age2)
+// let StringAge = String(age)
+// console.log(typeof StringAge)
+// let NumberAge = Number(age)
+// console.log(typeof NumberAge)
+// let booleanAge= Boolean(age)
+// console.log(booleanAge) 
+
+// let specialName = Symbol("sheikh")
+// let specialName1 = Symbol("sheikh")
+// let isEqual = specialName === specialName1
+// console.log(isEqual)
+
+//***********************************************//stack \\*************************************************** *//
+//all the premittive datatypes are stack because they did not effect the orignal variables they effect on the copy of it and orignal still 
+
+//ref(non-premittive)
+//object
+//array
+//functions
+
+// const userName = {
+// name:"sheikh",
+// emaiil:"sheikh@gmail.com",
+// rollNo:13323,
+// }
+// console.log(userName)
+
+// const name = ["sheikh","hasseb","king"]
+// console.log(name)
+
+// function sum(a,b){
+//     console.log(a * b)
+// }
+// sum(20,10)
+
+//*******************************************//Heap\\***************************************************************** */
+//all the refrence or non-premittive datatypes are heap type 
+//because the use the full refrence of that datatype
+//that all for today see you later// 

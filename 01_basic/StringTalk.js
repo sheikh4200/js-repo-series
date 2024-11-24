2b1e5ba7b87a2c33a662ab0941dfd0a4ad3c24cf
sheikh4200 <159758352+sheikh4200@users.noreply.github.com>
2024-11-24 12:19:24 +0000

    Talk deep about String

---
+++ 01_basic/StringTalk.js
@@ -0,0 +1,26 @@
+//topic to clear about string,
+//let start
+//as we  alredy knows that how to console a string //
+const name = "sheikh";
+const age = 25.9;
+// console.log(name + "  " + age)// this is the old way to console a string//
+console.log(`my name is ${name} and my age is ${age}`);// this the modren way to console a string
+
+const myGaming = new String("my name is rizwan")
+const hello =  "   hey    "
+const url ="https://sheikh%20rizwan" ;
+const split = "hello-world-md"
+console.log(myGaming.length)
+console.log(myGaming.charAt(11))
+console.log(myGaming.indexOf("r"))
+console.log(myGaming.substring(3,7))//it only work on positive number or value
+console.log(myGaming.slice(-17,-12 )) // it can work on both neitive and positive value or number
+console.log(hello.trim())// it erase the space n string // there are also trim-start and trim-end
+console.log(url.replace("%20","-"))//it is used to replace something you want in the string
+console.log(myGaming.includes("is"))//to find is this keywords exist in string
+console.log(split.split("-"))//to make string in to array
+
+
+
+
+

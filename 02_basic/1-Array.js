ff2df5e215ba49eaae2655b2c418d4ad86c2401a
sheikh4200 <159758352+sheikh4200@users.noreply.github.com>
2024-11-25 09:42:21 +0000

    Arrays

---
+++ 02_basic/1-Array.js
@@ -0,0 +1,23 @@
+//in this section we are gonig to start with array //
+//Array is also an object
+const arr = [1,2,3,4,5,]
+// console.log(arr[0])
+
+const myFriends = ["hasseb","hassam"]
+
+const arr2 = new Array("sheikh","rizwan")
+// console.log(arr2)
+
+//Array Methods//
+// arr.push(6)
+// console.log(arr)
+// arr.pop()
+// console.log(arr)
+// console.log(arr.length)
+// console.log(arr.unshift(0))
+// console.log(arr)
+// arr.shift(0)
+// console.log(arr)
+
+// console.log(arr.includes(4))
+

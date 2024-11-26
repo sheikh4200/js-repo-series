9465af2604f5e1e52d286a5a3322bbdf33bf4eef
sheikh4200 <159758352+sheikh4200@users.noreply.github.com>
2024-11-26 10:14:14 +0000

    add an important methods

--- 02_basic/1-Array.js
+++ 02_basic/1-Array.js
@@ -21,3 +21,16 @@ const arr2 = new Array("sheikh","rizwan")
 
 // console.log(arr.includes(4))
 
+//important method slice and splice
+//let talk about slice 
+const myarr = [0,1,2,3,4,5,6]
+console.log(myarr)
+const slice =myarr.slice(1,3)
+console.log(slice)
+console.log(myarr)
+// let tsalk about splice
+const splice = myarr.splice(1,3)
+console.log(splice)
+console.log(myarr)
+//thus: slice did not cut array in to two and did not effect on orignal array 
+//thus slpice cut array and make two arrays and also as effect on orignal array

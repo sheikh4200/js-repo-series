dcf550b09acd78f929ec2dda1e0dd18dbbd9ccf3
sheikh4200 <159758352+sheikh4200@users.noreply.github.com>
2024-11-30 07:24:08 +0000

    talk more about functions how they work with arrays and obejcts

--- 03_basic/functions.js
+++ 03_basic/functions.js
@@ -19,9 +19,43 @@
 
 //let practice a function with a logic example
 
-let isUserLoggedIn  = (username)=>{
-    if(username === undefined) return undefined;
-    return `${username} is logged in`
+// let isUserLoggedIn  = (username)=>{
+//     if(username === undefined) return undefined;
+//     return `${username} is logged in`
 
+// }
+// console.log(isUserLoggedIn("sheikh"))
+
+//*** part-2***//
+//learn about rest operators
+//for example
+// rest operators are also known as spreate operators depend on their usage
+// function addPriceCart(val,val2,...num1){
+// return num1
+// }
+// console.log(addPriceCart(12,13,14,15))//it return an Array
+
+//function with object
+
+// const user = {
+//     username:"sheikhRizwan",
+//     ages:25
+// }
+
+// function add(anytype){
+//     return `the username is ${anytype.username} and his age is ${anytype.age}`
+
+// }
+// console.log(add({
+//     username:"sheikh-rizwan",
+//     age:25
+// }))
+
+//functions with Array
+
+// let newArr = [1,2,3,4]
+
+function addArray(anytype){
+    return anytype[0]
 }
-console.log(isUserLoggedIn("sheikh"))
+console.log(addArray([1,2,3,4]))

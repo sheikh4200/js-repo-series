a351b926ccf8fe11cf2cb9f4443fe2edc94e53e1
sheikh4200 <159758352+sheikh4200@users.noreply.github.com>
2024-12-12 13:34:52 +0000

    if-else and else-if and switch codition talk

---
+++ 04_basic/if-else.js
@@ -0,0 +1,47 @@
+// if-else coditions
+//let start 
+
+let score = 30;
+
+// if(score >= 40){
+//     console.log("yes")
+// }
+
+// if(score >= 40){
+//     console.log("yes")
+// }else(
+//     console.log("not")
+// )
+
+//Else-if codition
+
+// let runs = 103;
+
+// if(runs > 300){
+//     console.log("runs is greater then 300")
+// }else if(runs > 500){
+//     console.log("runs is greater then 500")
+// }
+// else{
+//     console.log("runs is greater then 90")
+// }
+    
+//what when if in if codition
+//for example
+
+// let abc = true
+
+// if(true){
+//     console.log("no")
+//     if(true){
+//         console.log("invalid")
+//     if(true){
+//         console.log("Yes")
+//     }    
+//     }
+// }
+
+//truty and falsiy
+
+//truty : true ," ","-",{},[]
+//falsiy:0,-0,"",false,null,undefined,NaN
---
+++ 04_basic/switch.js
@@ -0,0 +1,17 @@
+//today we going to learn switch statement
+
+// let month = "march";
+
+// switch (month) {
+//     case "jan":
+//         console.log("yes its jan")
+//         break;
+//     case "feb":
+//         console.log("yes its feb")
+//         break;
+//         case "march":
+//             console.log("yes its march")
+//             break;
+//     default:
+//         break;
+// }
